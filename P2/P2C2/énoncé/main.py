defc55e8838e287db36e847e741a4ea8eb2a980a
diom-225 <diomandyouss1999@gmail.com>
2026-05-15 13:57:03 +0000

    Update main.py

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,31 @@
 # Ecrivez votre code ici !
+nombres = "1,2,3,4"
+liste = nombres.split(",")
+# Convertir les éléments en nonbre
+datas = []
+for i in liste:
+    if i == '1':
+       datas.append(1)
+    elif i == '2':
+       datas.append(2)
+    elif i == '3':
+      datas.append(3)
+    elif i == '4':
+      datas.append(4)
+print(datas)
+# Calculons la somme des nombres
+sommes = sum(datas)
+print(sommes)
+
+# Calculons la moyenne
+moyenne = sommes / len(datas)
+print(moyenne)
+
+# Calculons et affichons le nombre de nombres dans la liste qui sont supérieurs à la moyenne
+donnée = []
+for x in datas:
+   if x > moyenne:
+      donnée.append(x)
+      print(len(donnée))
+
+
